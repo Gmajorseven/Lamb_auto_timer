fee6043ad5d47f63e8b3e54471bb09b719358761
gmajorseven <guide.band8@gmail.com>
2025-04-13 16:36:05 +0700

    Update season time

--- Lamb_auto_timer.ino
+++ Lamb_auto_timer.ino
@@ -6,13 +6,10 @@ RTC_DS3231 rtc;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int relayPin = 13;
+const int relayPin = 8;
 
 DateTime now;
-const int on_hour = 17;
-const int on_mintue = 30;
-const int off_hour = 5;
-const int off_minute = 50;
+int on_hour, on_minute, off_hour, off_minute;
 
 void lightOn() {
   digitalWrite(relayPin, HIGH);
@@ -37,17 +34,14 @@ void setup() {
   Wire.begin();
 
   if(!rtc.begin()) {
-
     Serial.println("Couldn't find RTC!");
     while(1);
   }
-
   if(rtc.lostPower()) {
-
     Serial.println("RTC lost power, let's set the time!");
   }
 
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+ // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   lcd.begin(16,2);
   lcd.clear();
@@ -59,6 +53,24 @@ void loop() {
   now = rtc.now();
   int curent_hour = now.hour();
   int curent_minute = now.minute();
+  int current_month = now.month();
+  int current_hour = now.hour();
+  int current_minute = now.minute();
+
+  // Set time based on season
+  if (current_month >= 4 && current_month <= 9) {
+    // Summer (April to September)
+    on_hour = 18;
+    on_minute = 15;
+    off_hour = 5;
+    off_minute = 45;
+  } else {
+    // Winter (October to March)
+    on_hour = 17;
+    on_minute = 15;
+    off_hour = 5;
+    off_minute = 50;
+  }
 
   lcd.setCursor(0, 0);
   lcd.print("Time:");
@@ -73,31 +85,21 @@ void loop() {
   int off_time_in_munutes = off_hour * 60 + off_minute;
 
   if(on_time_in_minutes < off_time_in_munutes) {
-    
     if(current_time_in_minutes >= on_time_in_minutes && current_time_in_minutes < off_time_in_munutes) {
-      
       lightOn();
-
     } else {
       //test
       //lightOn();
       lightOff();
-
     }
-
   } else {
-
     if(current_time_in_minutes >= on_time_in_minutes || current_time_in_minutes < off_time_in_munutes) {
-
       lightOn();
-
     } else {
       //test
       //lightOn();
       lightOff();
-
     }
-
   }
 
   Serial.print(now.hour());
@@ -107,5 +109,4 @@ void loop() {
   Serial.println(now.second());
 
   delay(1000);
-
 }
